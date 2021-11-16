0d9f57a300a03aceb29ae6afc236befadf440355
Lîm Tsú-thuàn≡悃悃≡林子篆 <dannypsnl@gmail.com>
2021-11-16 14:36:36 +0800

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "racket-project")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/racket-project.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define license '(Apache-2.0 OR MIT))
+(define pkg-authors '(dannypsnl))
